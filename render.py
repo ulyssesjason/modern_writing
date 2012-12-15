24acd141f35c82d343ddb3e4ba103a037e6543d7
ulyssesjason <ulyssesjason@gmail.com>
2012-12-14 19:58:04 -0600

    Simple kpi simulation phase 0.1
    input one number, display 5 similiar number

--- render.py
+++ render.py
@@ -24,6 +24,14 @@ class BlogHandler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+
+class PluginHandler(BaseHandler):
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
 def render_post(response, post):
     response.out.write('<b>' + post.subject + '</b><br>')
     response.out.write(post.content)
